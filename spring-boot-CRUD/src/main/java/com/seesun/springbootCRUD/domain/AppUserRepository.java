5ab45e948c811385e79d03398f0fece43596f76a
dbseend <141024382+dbseend@users.noreply.github.com>
2024-02-19 01:39:47 +0900

    Delete spring-boot-CRUD directory

--- spring-boot-CRUD/src/main/java/com/seesun/springbootCRUD/domain/AppUserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.seesun.springbootCRUD.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-// JpaRepository를 상속받아 쿼리문을 작성하지 않아도 기본적인 CRUD가 가능하다.
-public interface AppUserRepository extends JpaRepository<AppUser, Long> {
-}
